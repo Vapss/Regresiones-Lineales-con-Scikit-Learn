5ffb48ae2d94b53c20ae424c608eec26643e0cb0
José Antonio Portuguez Vázquez <65097050+Vapss@users.noreply.github.com>
2022-03-17 20:55:31 -0600

    Nuevas Partes
    Nuevas Partes

--- correlacion_variables.py
+++ correlacion_variables.py
@@ -4,9 +4,12 @@ import seaborn as sns
 
 
 #Lee el corpus original del archivo de entrada y lo pasa a una DataFrame
-df = pd.read_csv('dataset_ejercicio_I_regresion_lineal.csv', sep=',', engine='python')
+df = pd.read_csv('cal_housing.csv', sep=',', engine='python')
 
-X = df['num_bedrooms'].values
+for i in len(df):
+    print(df.columns[i])
+
+X = df[i].values
 y = df['price'].values
 plt.scatter(X,y)
 plt.xlabel('num_bedrooms')
@@ -15,7 +18,7 @@ plt.ylabel('price')
 #~ plt.show()
 
 #~ #Correlación 
-#~ X = df.drop(['price'],axis=1).values   
+#~ X = df.drop(['price'],axis=1).values
 #~ print (X)
 
 correlation_matrix = df.corr().round(2)
